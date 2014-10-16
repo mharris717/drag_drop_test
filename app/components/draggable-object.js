65d390177c90c95deccae7fa126d6e9071a5c691
Mike Harris <mharris717@gmail.com>
2014-10-16 09:31:27 -0400

    drag and remove works

--- app/components/draggable-object.js
+++ app/components/draggable-object.js
@@ -9,12 +9,14 @@ export default Ember.Component.extend({
     var dataTransfer = event.originalEvent.dataTransfer;
 
     var obj = this.get('content');
-    var id = this.get('coordinator').setObject(obj);
+    var id = this.get('coordinator').setObject(obj, {source: this});
 
     dataTransfer.setData('Text', id);
   }.on("dragStart"),
 
-  notifyDrop: function() {
+  notifyDrop: function(obj) {
+    console.debug("notifyDrop");
+    this.get("parent").removeObject(obj);
     // this.incrementProperty("dropNotifyCount");
   }
 });
