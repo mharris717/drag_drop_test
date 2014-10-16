e52470254b0a0f063be5c305cdae44e2bca6f383
Mike Harris <mharris717@gmail.com>
2014-10-16 11:33:11 -0400

    CS

--- app/components/object-bin.js
+++ app/components/object-bin.js
@@ -5,5 +5,25 @@ export default Ember.Component.extend({
 
   forEach: function(f) {
     return this.get('model').forEach(f);
+  },
+
+  _yield: function(context, options) {
+    var view = options.data.view;
+    var parentView = this._parentView;
+    var template = Ember.get(this, 'template');
+
+    if (template) {
+      Ember.assert("A Component must have a parent view in order to yield.", parentView);
+
+      view.appendChild(Ember.View, {
+        isVirtual: true,
+        tagName: '',
+        _contextView: parentView,
+        template: template,
+        context: Ember.get(view, 'context'),
+        controller: Ember.get(view, 'controller'),
+        templateData: { keywords: {} }
+      });
+    }
   }
 });
--- tests/unit/components/object-bin-test.js
+++ tests/unit/components/object-bin-test.js
@@ -8,13 +8,44 @@ moduleForComponent("object-bin", 'ObjectBinComponent', {
 });
 
 test("smoke", function() {
-  equal(2,2);
-
   var obj = Thing.create({title: "Hello"});
   var all = [obj];
-  var s = this.subject({model: all});
+  var s = this.subject({
+    model: all,
+    template: Ember.Handlebars.compile("<span class='title'>{{title}}</span>")
+  });
 
   var titles = this.$().find(".title");
+  equal(titles.length,1);
+  equal(titles.text().trim(),"Hello");
+});
+
+test("component on fly 1", function() {
+  var obj = Thing.create({title: "Hello"});
+  var all = [obj];
+
+  var s = this.subject({
+    layout: Ember.Handlebars.compile("{{#object-bin model=all}}{{yield}}{{/object-bin}}"),
+    template: Ember.Handlebars.compile("<span class='title'>{{title}}</span>"),
+    all: all
+  });
+
+  var titles = this.$().find(".title");
+  equal(titles.length,1);
+  equal(titles.text().trim(),"Hello");
+});
+
+test("component on fly 2", function() {
+  var obj = Thing.create({title2: "Hello"});
+  var all = [obj];
+
+  var s = this.subject({
+    layout: Ember.Handlebars.compile("{{#object-bin model=all title2='notit'}}<span class='title2'>{{title2}}</span>{{/object-bin}}"),
+
+    all: all
+  });
+
+  var titles = this.$().find(".title2");
   equal(titles.length,1);
   equal(titles.text().trim(),"Hello");
 });
